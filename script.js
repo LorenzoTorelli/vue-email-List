0bbbefa5b3ef763e57614babf2279ffc39140104
LorenzoTorelli <lorenzo.torelli98@gmail.com>
2021-12-09 16:55:53 +0100

    esercizio

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const app = new Vue (
     {
         el: '#root',
-        data: {},
-        methods: {}
+        data: {
+            elemento: [],
+        },
+        methods: {
+           
+        },
+        created() {
+            for (let i = 0; i<10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) =>  {
+                        // handle success
+                    this.elemento.push(response.data.response);
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    })
+            }
+        }
     }
 )
